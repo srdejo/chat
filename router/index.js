93b7a05a8c51a8cfd964ddda7c2833a69ac08114
Daniel Jimenez <srdejo@gmail.com>
2015-05-22 15:53:41 -0500

    Chat01
    Chat

---
+++ router/index.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const st = require('st')
+
+console.log('entra')
+
+const mount = st({
+	path: path.join(__dirname, '..', 'public'),
+	index: 'index.html'
+})
+
+function onRequest(req, res){
+	mount(req, res, function(err){
+		if(err) return res.end(err.message)
+
+	res.statusCode = 404
+	res.end(`404 Not Found: ${req.url}`)
+	})
+}
+
+module.exports = onRequest
